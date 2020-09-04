09f337e2f6150955b7c8d1d2eb209f76491abcd0
Rafael Martins <rafaeldcmartins@gmail.com>
2020-09-03 21:46:43 -0300

    Feat: image gallery

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -168,7 +168,7 @@ const PhotosUpload = {
         if (totalPhotos > uploadLimit) {
             alert(`Você atingiu o limite máximo de ${uploadLimit} imagens!`);
             event.preventDefault();
-            
+
             return true;
         }
 
@@ -217,3 +217,33 @@ const PhotosUpload = {
         photoDiv.remove();
     }
 }
+
+const ImageGallery = {
+    highlight: document.querySelector('.gallery .highlight > img'),
+    previews: document.querySelectorAll('.gallery .gallery-preview img'),
+    setImage(event) {
+        const { target } = event;
+        ImageGallery.previews.forEach(preview => preview.classList.remove('active-image'));
+        target.classList.add('active-image');
+        ImageGallery.highlight.src = target.src;
+        Lightbox.image.src = target.src;
+    }
+};
+
+const Lightbox = {
+    target: document.querySelector('.lightbox-target'),
+    image: document.querySelector('.lightbox-target img'),
+    closeButton: document.querySelector('.lightbox-target a.lightbox-close'),
+    open() {
+        Lightbox.target.style.opacity = 1;
+        Lightbox.target.style.top = 0;
+        Lightbox.target.style.bottom = 0;
+        Lightbox.closeButton.style.top = 0;
+    },
+    close() {
+        Lightbox.target.style.opacity = 0;
+        Lightbox.target.style.top = '-100%';
+        Lightbox.target.style.bottom = 'initial';
+        Lightbox.closeButton.style.top = '-80px';
+    }
+};
