300937396fc928584c187866959c1bb581d8e597
Rafael Martins <rafaeldcmartins@gmail.com>
2020-09-22 22:01:29 -0300

    Login page

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,5 @@
+module.exports = {
+    loginForm(req, res) {
+        return res.render('session/login');
+    }
+}
--- src/server.js
+++ src/server.js
@@ -18,6 +18,6 @@ nunjucks.configure('src/app/views', {
     noCache: true
 });
 
-server.listen(3000, function () {
-    console.log('server is running on port 3000');
+server.listen(5000, function () {
+    console.log('server is running on port 5000');
 });
