055f94cf0fad15a079e10777b1e750fc3efba7ba
Rafael Martins <rafaeldcmartins@gmail.com>
2020-09-29 19:12:34 -0300

    Add email template

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,6 +1,7 @@
 const User = require('../models/User');
 const crypto = require('crypto');
 const mailer = require('../../lib/mailer');
+const { emailTemplate } = require('../../lib/utils');
 
 module.exports = {
     registerForm(req, res) {
@@ -8,47 +9,37 @@ module.exports = {
     },
     async post(req, res) {
         try {
-            const keys = Object.keys(req.body);
-
-            for (let key of keys) {
-                if (req.body[key] == '')
-                    return res.send('Por favor, preencha todos os campos.');
-            }
-
-            // check if user already exists
-            const { email, name } = req.body;
-            const user = await User.findOne(email);
-
-            if (user) return res.send('Este usuário já está cadastrado!');
-            
-            const password = crypto.randomBytes(6).toString('hex');
-            const data = {
-                ...req.body,
-                password
-            };
-
-            await mailer.sendMail({
-                to: email,
-                from: 'no-reply@foodfy.com.br',
-                subject: 'Bem-vindo ao Foodfy',
-                html: `
-                <h2>Olá <strong>${name}</strong>,</h2>
-                <p>Seja muito bem-vindo ao <strong>Foodfy</strong> :)</p>
+            const password = crypto.randomBytes(3).toString('hex');
+            const welcomeEmail = `
+                <h2 style="font-size: 24px; font-weight: normal;">Olá <strong>${req.body.name}</strong>,</h2>
+                <p>Seja muito bem-vindo(a) ao <strong>Foodfy</strong> :)</p>
                 <p>Seu cadastro foi realizado com sucesso! Confira seus dados:</p>
-                <p>Login: ${email}</p>
+                <p>Login: ${req.body.email}</p>
                 <p>Senha: ${password}</p>
                 <br>
                 <h3>Como eu acesso minha Conta?</h3>
                 <p>
-                    Bem simples, você só precisa clicar
-                    <a href="http:localhost:5000/admin/users/login" target="_blank">aqui</a> e entrar com seu email e senha informados acima.
-                </p>
-                <br><br>
-                <p>Te esperamos lá!</p>
-                <p>Equipe Foodfy.<p>
-                `
+                    Bem simples, você só precisa clicar no botão abaixo e entrar com seu email e senha informados acima.
+				</p>
+				<p style="text-align: center;">
+                    <a
+                        style="display: block; margin: 32px auto; padding: 16px; width:150px; color: #fff;
+                        background-color: #6558C3; text-decoration: none; border-radius: 4px;"
+                        href="http:localhost:5000/admin/users/login" target="_blank"
+                    >Acessar</a> 
+				</p>
+                <p style="padding-top:16px; border-top: 2px solid #ccc">Te esperamos lá!</p>
+                <p>Equipe Foodfy.</p>
+            `;
+
+            await mailer.sendMail({
+                to: req.body.email,
+                from: 'no-reply@foodfy.com.br',
+                subject: 'Bem-vindo ao Foodfy',
+                html: emailTemplate(welcomeEmail)
             });
 
+            const data = { ...req.body, password };
             const userID = await User.create(data);
 
             return res.send('Success!');
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,10 +1,40 @@
 module.exports = {
-    date(timestamp) {
-        const date = new Date(timestamp);
-        const year = date.getUTCFullYear();
-        const month = `0${date.getUTCMonth() + 1}`.slice(-2);
-        const day = `0${date.getUTCDate()}`.slice(-2);
-
-        return `${year}-${month}-${day}`;
+    emailTemplate(content) {
+        return `
+        <body style="margin:0;padding:0;font-family: helvetica;color:#444;">
+            <table width="100%" align="center" cellpadding="0" cellspacing="0" style="max-width:600px;">
+                <tr>
+                    <td style="padding:20px 0;" bgcolor="#111" align="center">
+                        <img
+                            style="display:block;"
+                            alt="Logo Foodfy"
+                            src="https://raw.githubusercontent.com/martins-rafael/foodfy/master/public/images/logo_admin.png"
+                        />
+                    </td>
+                </tr>
+                <tr>
+                    <td style="padding:20px;">
+                        <table width="100%" cellpadding="0" cellspacing="0">
+                            <tr>
+                                <td style="padding:30px 0;">
+                                    ${content}
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+                
+                <tr>
+                    <td style="padding:15px 10px 15px 10px;" bgcolor="#eee">
+                        <table width="100%" cellpadding="0" cellspacing="0">
+                            <tr>
+                                <td width="480" style="color:#999;" align="center">Todos direitos reservados, receitas Foodfy</td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+        </body>
+        `;
     }
 };
